3fde8619b1682c077708788609a43b4f76fa06ef
thenatural86 <philip.smalls86@gmail.com>
2020-11-12 15:59:00 -0500

    local storage

--- counter.js
+++ counter.js
@@ -1,16 +1,29 @@
-let counter = 0;
+// check if there is already a value in local storage
+if(!localStorage.getItem('counter')){
+    // if not, ser the counter to 0 in local storage
+    localStorage.setItem('counter', 0)
+}
+
 
 count = () => {
+    // retrieve counter value from local storage
+    let counter = localStorage.getItem('counter');
+    // update counter
     counter++;
     document.querySelector('h1').innerHTML = counter;
 
-    if (counter % 10 === 0){
-        alert(`Count is now ${counter}`)
-    }
+    // if (counter % 10 === 0){
+    //     alert(`Count is now ${counter}`)
+    // }
+    // store in local storage
+    localStorage.setItem('counter', counter);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    // set heading to the current value inside local storage
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+
     document.querySelector('button').onclick = count;
 
-    setInterval(count, 1000)
+    // setInterval(count, 1000)
 });
